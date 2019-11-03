cf4dfc8e032e0e0addcce281309d5f41558cfa70
Daniel J. Rollins <daniel@djrollins.com>
2019-11-03 09:44:31 +0000

    add addition module

---
+++ src/14-testing/addition/src/Addition.hs
@@ -0,0 +1,18 @@
+module Addition where
+
+import           Test.Hspec
+
+multiply :: (Eq a, Num a) => a -> a -> a
+multiply x y | x == 0 || y == 0 = 0
+             | x == 1           = y
+             | y == 1           = x
+             | otherwise        = multiply (x - 1) (y + y)
+
+main :: IO ()
+main = hspec $ describe "multiply" $ do
+        it "evaluates to 0 if x is 0" $ multiply 0 2 `shouldBe` (0 :: Int)
+        it "evaluates to 0 if y is 0" $ multiply 2 0 `shouldBe` (0 :: Int)
+        it "evaluates to y if x is 1" $ multiply 2 0 `shouldBe` (0 :: Int)
+        it "evaluates to y if x is 1" $ multiply 1 10 `shouldBe` (10 :: Int)
+        it "evaluates to x if y is 1" $ multiply 10 1 `shouldBe` (10 :: Int)
+        it "multiply 3 7 == 21" $ multiply 3 7 `shouldBe` (21 :: Int)
